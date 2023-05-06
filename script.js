4cbedbc2d66a1dca834196dc7c4901660436521d
BennoA <till.benno.knipp@web.de>
2023-05-06 18:30:04 +0200

    created some htmls with imgs and buttons, css and js

---
+++ script.js
@@ -0,0 +1,15 @@
+
+const catButton = document.getElementById("cat-button");
+catButton.addEventListener("click", function () {
+  window.location.href = "cat.html";
+});
+
+const dogButton = document.getElementById("dog-button");
+dogButton.addEventListener("click", function () {
+  window.location.href = "dog.html";
+});
+
+const learnButton = document.getElementById("learn-button");
+learnButton.addEventListener("click", function () {
+  window.location.href = "learn.html";
+});
